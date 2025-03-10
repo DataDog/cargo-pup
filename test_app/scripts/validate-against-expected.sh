ed8df681f363bebb85c66978d39143e6682ccc0a
Scott Gerring <scott@scottgerring.com>
2025-03-10 15:56:15 +0100

    CI test to confirm lint output unchanged

--- test_app/scripts/validate-against-expected.sh
+++ test_app/scripts/validate-against-expected.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+export PATH="$(cd "$(dirname "$0")/../../target/release" && pwd):$(cd "$(dirname "$0")/../../target/debug" && pwd):$PATH"
+pushd "$(dirname "$0")/../"
 cargo pup 2>&1 | grep -v 'Finished' | diff - expected_output
+popd
