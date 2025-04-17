e39b383c305afd2a1a3684268507d853885fa75a
Scott Gerring <scott@scottgerring.com>
2025-04-17 11:31:35 +0200

    make test_app testing more robust

--- test_app/scripts/update-expected-output.sh
+++ test_app/scripts/update-expected-output.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export PATH="$(cd "$(dirname "$0")/../../target/release" && pwd):$(cd "$(dirname "$0")/../../target/debug" && pwd):$PATH"
-pushd "$(dirname "$0")/../"
-cargo pup 2>&1 | grep -v 'Finished' > expected_output
-popd
+PATHS=""
+[ -d "$(dirname "$0")/../../target/release" ] && PATHS="$(cd "$(dirname "$0")/../../target/release" && pwd):$PATHS"
+[ -d "$(dirname "$0")/../../target/debug" ] && PATHS="$(cd "$(dirname "$0")/../../target/debug" && pwd):$PATHS"
+export PATH="${PATHS}$PATH"
+
+pushd "$(dirname "$0")/../" > /dev/null
+cargo pup 2>&1 | grep -v 'Finished' | grep -vE '^\s*Checking .* \(\/Users\/.*\)' > expected_output
+popd > /dev/null
--- test_app/scripts/validate-against-expected.sh
+++ test_app/scripts/validate-against-expected.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export PATH="$(cd "$(dirname "$0")/../../target/release" && pwd):$(cd "$(dirname "$0")/../../target/debug" && pwd):$PATH"
+PATHS=""
+[ -d "$(dirname "$0")/../../target/release" ] && PATHS="$(cd "$(dirname "$0")/../../target/release" && pwd):$PATHS"
+[ -d "$(dirname "$0")/../../target/debug" ] && PATHS="$(cd "$(dirname "$0")/../../target/debug" && pwd):$PATHS"
+export PATH="${PATHS}$PATH"
+
 pushd "$(dirname "$0")/../" > /dev/null
-cargo pup 2>&1 | grep -v 'Finished' | diff - expected_output
+cargo pup 2>&1 | grep -v 'Finished' | grep -vE '^\s*Checking .* \(\/Users\/.*\)' | diff - expected_output
 if [ $? -ne 0 ]; then
   popd > /dev/null
   exit -1
