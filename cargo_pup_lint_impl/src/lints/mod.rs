3bad7ce6e8194f010bd38fb8450d31ebf1d01740
Scott Gerring <scott@scottgerring.com>
2025-05-12 16:26:14 +0200

    Starting to flush out new lint structure

--- cargo_pup_lint_impl/src/lints/mod.rs
+++ cargo_pup_lint_impl/src/lints/mod.rs
@@ -1,1 +1,37 @@
+use cargo_pup_lint_config::ConfiguredLint;
+use crate::ArchitectureLintRule;
+
+use rustc_lint::{LateContext, LateLintPass, Lint, LintStore};
+
 pub mod configuration_factory;
+mod module;
+
+pub struct ModuleLint {
+    config: ConfiguredLint::Module
+}
+
+impl ModuleLint {
+    pub fn new(config: ConfiguredLint::Module) -> Self {
+        Self {
+            config
+        }
+    }
+}
+
+impl ArchitectureLintRule for ModuleLint {
+    fn name(&self) -> String {
+        todo!()
+    }
+
+    fn applies_to_module(&self, namespace: &str) -> bool {
+        todo!()
+    }
+
+    fn applies_to_trait(&self, _trait_path: &str) -> bool {
+        todo!()
+    }
+
+    fn register_late_pass(&self, _lint_store: &mut LintStore) {
+        todo!()
+    }
+}
